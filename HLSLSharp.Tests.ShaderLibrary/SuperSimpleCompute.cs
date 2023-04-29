fa5733c7014952252f87cf3643789e128626f6d6
chair <projectaa76@gmail.com>
2023-04-29 12:09:11 -0400

    Re-arrange project a bit Add `Tests.ShaderLibrary` project

---
+++ HLSLSharp.Tests.ShaderLibrary/SuperSimpleCompute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Shaders;
+
+namespace HLSLSharp.Tests.ShaderLibrary;
+
+[ComputeShader(1, 1, 1)]
+public partial struct SuperSimpleCompute : IComputeShader
+{
+    [Kernel]
+    public void Compute()
+    {
+        Vector3I threadIdCopy = ThreadId;
+
+        Vector2I threadIdXY = (Vector2I)ThreadId.XY;
+
+        int threadIdX = ThreadId.X;
+    }
+}
